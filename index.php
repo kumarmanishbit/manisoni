00eaa0b3315f8b3f2332913d5c986af5884216b2
Manish Kumar <kumarmanishbit@rediffmail.com>
2022-01-20 10:32:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
     <a class="nav-link disabled">About</a>
   </li>
 </ul>
+<script bronto-popup-id="05fc51f2-610b-4a02-8f22-67d610bdc4e4" src="https://cdn.bronto.com/popup/delivery.js"></script>
 
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
   <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
